15240144988904de64179befa6d2a55ea87266e7
Opie Taylor <taborn.nequil@gmail.com>
2025-01-29 09:45:14 -0600

    guess

--- src/GuessControl.jsx
+++ src/GuessControl.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
-class GuessControl extends Component {
+class GuessControlOld extends React.Component {
   constructor(props) {
     super(props);
 
@@ -9,10 +9,6 @@ class GuessControl extends Component {
       currentGuess: "",
     };
 
-    /**
-     * These lines are required to make the methods/functions declared on this
-     *  class have the correct `this` object when they run.
-     */
     this.handleInputChange = this.handleInputChange.bind(this);
     this.onSubmitGuess = this.onSubmitGuess.bind(this);
   }
@@ -22,10 +18,6 @@ class GuessControl extends Component {
   }
 
   onSubmitGuess() {
-    // Since the values from an HTML input are strings by default,
-    //  convert to a number for the returned guess value
-    //  by passing in the string to the Number function.
-    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
     this.props.onGuess(Number(this.state.currentGuess));
     this.setState({ currentGuess: "" });
   }
@@ -43,5 +35,30 @@ class GuessControl extends Component {
     );
   }
 }
+
+// New functional component version
+const GuessControl = ({ onGuess }) => {
+  const [currentGuess, setCurrentGuess] = useState(""); 
+
+  const handleInputChange = (event) => {
+    setCurrentGuess(event.target.value); 
+  };
+
+  const onSubmitGuess = () => {
+    onGuess(Number(currentGuess)); 
+    setCurrentGuess(""); 
+  };
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={currentGuess}
+        onChange={handleInputChange} 
+      />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
+};
 
 export default GuessControl;
